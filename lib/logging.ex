cef4dac0b35a8e8e3062d4ec5f30537b17362eae
Mayel de Borniol <git@mayel.space>
2022-12-09 14:13:32 +1300

    OnePlusNDetector

--- lib/logging.ex
+++ lib/logging.ex
@@ -7,6 +7,10 @@ defmodule Bonfire.Logging do
     EctoSparkles.Log.setup(repo_module)
     # Ecto.DevLogger.install(repo_module)
 
+    if Code.ensure_loaded?(Mix) and Mix.env() == :dev do
+      OnePlusNDetector.setup(repo_module)
+    end
+
     setup_oban()
   end
 
