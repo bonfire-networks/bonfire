27bb7e4475c59f2a0c7878d8a0dda1eb1bdbadc8
Mayel de Borniol <git@mayel.space>
2022-12-09 14:49:07 +1300

    Fix changelog?

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Bonfire.Spark.MixProject do
     ++
     [
       app: :bonfire,
-      version: "0.1.0",
+      version: "0.1.0-beta.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
