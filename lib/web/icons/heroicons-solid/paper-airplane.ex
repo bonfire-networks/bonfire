faa82b20a398a7e2589b6eee907c30181cd98f00
ivanminutillo <ivanminutillo@outlook.it>
2023-01-06 11:27:08 +0100

    icons

---
+++ lib/web/icons/heroicons-solid/paper-airplane.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.HeroiconsSolid.PaperAirplane do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" d="M10.894 2.553a1 1 0 0 0-1.788 0l-7 14a1 1 0 0 0 1.169 1.409l5-1.429A1 1 0 0 0 9 15.571V11a1 1 0 1 1 2 0v4.571a1 1 0 0 0 .725.962l5 1.428a1 1 0 0 0 1.17-1.408l-7-14Z"/></svg>
+    """
+  end
+end
