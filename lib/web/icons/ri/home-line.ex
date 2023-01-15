025ccafaf26796a739666998faff9b832679f9e8
ivanminutillo <ivanminutillo@outlook.it>
2023-01-15 18:19:18 +0100

    icons

---
+++ lib/web/icons/ri/home-line.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.Ri.HomeLine do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 24 24" aria-hidden="true"><path fill="currentColor" d="M21 20a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.49a1 1 0 0 1 .386-.79l8-6.222a1 1 0 0 1 1.228 0l8 6.222a1 1 0 0 1 .386.79V20zm-2-1V9.978l-7-5.444l-7 5.444V19h14z"/></svg>
+    """
+  end
+end
