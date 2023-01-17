4c2906fe07a4a7c376b02ab4e149c25e9afa0e69
ivanminutillo <ivanminutillo@outlook.it>
2023-01-17 16:40:00 +0100

    ui

---
+++ lib/web/icons/heroicons-outline/dots-horizontal.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.HeroiconsOutline.DotsHorizontal do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 24 24" aria-hidden="true"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 1 1-2 0a1 1 0 0 1 2 0Zm7 0a1 1 0 1 1-2 0a1 1 0 0 1 2 0Zm7 0a1 1 0 1 1-2 0a1 1 0 0 1 2 0Z"/></svg>
+    """
+  end
+end
