c227a7ba0652d274dba6af09821ed10473485ef3
ivanminutillo <ivanminutillo@outlook.it>
2023-01-18 18:28:49 +0100

    ui

---
+++ lib/web/icons/lucide/paintbrush-2.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.Lucide.Paintbrush2 do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 24 24" aria-hidden="true"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 19.9V16h3a2 2 0 0 0 2-2v-2H5v2c0 1.1.9 2 2 2h3v3.9a2 2 0 1 0 4 0ZM6 12V2h12v10M14 2v4m-4-4v2"/></svg>
+    """
+  end
+end
