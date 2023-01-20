c988a4fdc859edf3d01dd5878a88a97cab78b25a
ivanminutillo <ivanminutillo@outlook.it>
2023-01-20 17:13:18 +0100

    main routes

--- lib/web/router.ex
+++ lib/web/router.ex
@@ -79,7 +79,7 @@ defmodule Bonfire.Web.Router do
 
     # TODO: make the homepage non-live
     live("/", Bonfire.Web.HomeLive, as: :home, private: %{cache: true})
-    live("/about/:tab", Bonfire.Web.HomeLive, private: %{cache: true})
+    live("/about", Bonfire.Web.HomeLive, private: %{cache: true})
     live("/terms", Bonfire.Web.TermsLive, private: %{cache: true})
     live("/conduct", Bonfire.Web.CodeOfConductLive, private: %{cache: true})
     live("/changelog", Bonfire.Web.ChangelogLive, private: %{cache: true})
