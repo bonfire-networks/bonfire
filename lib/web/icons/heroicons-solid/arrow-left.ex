0b7dfe20cc537b0529a48d751b1c84a8160865f2
Mayel de Borniol <git@mayel.space>
2023-01-26 13:27:28 +1300

    f

--- lib/web/icons/heroicons-solid/arrow-left.ex
+++ lib/web/icons/heroicons-solid/arrow-left.ex
@@ -1,8 +1,23 @@
 defmodule Iconify.HeroiconsSolid.ArrowLeft do
   use Phoenix.Component
+
   def render(assigns) do
     ~H"""
-    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" fill-rule="evenodd" d="M9.707 16.707a1 1 0 0 1-1.414 0l-6-6a1 1 0 0 1 0-1.414l6-6a1 1 0 0 1 1.414 1.414L5.414 9H17a1 1 0 1 1 0 2H5.414l4.293 4.293a1 1 0 0 1 0 1.414Z" clip-rule="evenodd"/></svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      role="img"
+      class={@class}
+      viewBox="0 0 20 20"
+      aria-hidden="true"
+    >
+      <path
+        fill="currentColor"
+        fill-rule="evenodd"
+        d="M9.707 16.707a1 1 0 0 1-1.414 0l-6-6a1 1 0 0 1 0-1.414l6-6a1 1 0 0 1 1.414 1.414L5.414 9H17a1 1 0 1 1 0 2H5.414l4.293 4.293a1 1 0 0 1 0 1.414Z"
+        clip-rule="evenodd"
+      />
+    </svg>
     """
   end
 end
