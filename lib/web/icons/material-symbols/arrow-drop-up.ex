80c00ff60b44daa509fe0e0e8253d59627b99a51
ivanminutillo <ivanminutillo@outlook.it>
2023-01-26 17:15:53 +0100

    ui

---
+++ lib/web/icons/material-symbols/arrow-drop-up.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.MaterialSymbols.ArrowDropUp do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 24 24" aria-hidden="true"><path fill="currentColor" d="m7 14l5-5l5 5Z"/></svg>
+    """
+  end
+end
