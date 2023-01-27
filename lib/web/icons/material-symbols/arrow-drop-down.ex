ded9b89a299af1fb294bf01013083e1a72ab5b04
Mayel de Borniol <git@mayel.space>
2023-01-27 14:37:28 +1300

    f

--- lib/web/icons/material-symbols/arrow-drop-down.ex
+++ lib/web/icons/material-symbols/arrow-drop-down.ex
@@ -1,8 +1,18 @@
 defmodule Iconify.MaterialSymbols.ArrowDropDown do
   use Phoenix.Component
+
   def render(assigns) do
     ~H"""
-    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 24 24" aria-hidden="true"><path fill="currentColor" d="m12 15l-5-5h10Z"/></svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      role="img"
+      class={@class}
+      viewBox="0 0 24 24"
+      aria-hidden="true"
+    >
+      <path fill="currentColor" d="m12 15l-5-5h10Z" />
+    </svg>
     """
   end
 end
