21949af0e14ab78b556f09859a81d25b9df87276
ivanminutillo <ivanminutillo@outlook.it>
2023-02-03 18:31:01 +0100

    icons

---
+++ lib/web/icons/clarity/picture-solid.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.Clarity.PictureSolid do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 36 36" aria-hidden="true"><path fill="currentColor" d="M32 4H4a2 2 0 0 0-2 2v24a2 2 0 0 0 2 2h28a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2ZM8.92 8a3 3 0 1 1-3 3a3 3 0 0 1 3-3ZM6 27v-4.1l6-6.08a1 1 0 0 1 1.41 0L16 19.35L8.32 27Zm24 0H11.15l6.23-6.23l5.4-5.4a1 1 0 0 1 1.41 0L30 21.18Z" class="clr-i-solid clr-i-solid-path-1"/><path fill="none" d="M0 0h36v36H0z"/></svg>
+    """
+  end
+end
