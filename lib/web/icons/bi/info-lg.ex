71011a5b1b486cbfdf09a7bb92a742e22fb92412
ivanminutillo <ivanminutillo@outlook.it>
2023-02-06 08:34:43 +0100

    icon

---
+++ lib/web/icons/bi/info-lg.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.Bi.InfoLg do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 16 16" aria-hidden="true"><path fill="currentColor" d="m9.708 6.075l-3.024.379l-.108.502l.595.108c.387.093.464.232.38.619l-.975 4.577c-.255 1.183.14 1.74 1.067 1.74c.72 0 1.554-.332 1.933-.789l.116-.549c-.263.232-.65.325-.905.325c-.363 0-.494-.255-.402-.704l1.323-6.208Zm.091-2.755a1.32 1.32 0 1 1-2.64 0a1.32 1.32 0 0 1 2.64 0Z"/></svg>
+    """
+  end
+end
