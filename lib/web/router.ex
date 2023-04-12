39e57b24383b787c60bcd6a44b3d73cc7f664ef8
Mayel de Borniol <git@mayel.space>
2023-04-12 14:16:53 +1200

    https://github.com/bonfire-networks/bonfire-app/issues/516

--- lib/web/router.ex
+++ lib/web/router.ex
@@ -29,6 +29,8 @@ defmodule Bonfire.Web.Router do
 
   # use_if_enabled Bonfire.Website.Web.Routes
 
+  use_if_enabled(Bonfire.Files.Routes)
+
   use_if_enabled(Bonfire.UI.Social.Routes)
   use_if_enabled(Bonfire.Boundaries.Web.Routes)
 
