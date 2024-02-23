b3822bd1afcb3a145d6eac2bae9f7cbf9e6bd163
Mayel de Borniol <git@mayel.space>
2024-02-23 18:44:23 +0000

    db optional

--- config/test.exs
+++ config/test.exs
@@ -32,20 +32,22 @@ config :bonfire, Bonfire.Web.Endpoint,
   ]
 
 maybe_repo_ipv6 = if System.get_env("ECTO_IPV6") in yes?, do: [:inet6], else: []
-repo_connection_config =
-  if System.get_env("DATABASE_URL") do
+repo_connection_config = cond do
+   System.get_env("DATABASE_URL") = db_url ->
     [
-      url: System.get_env("DATABASE_URL"),
+      url: db_url,
       socket_options: maybe_repo_ipv6
     ]
-  else
+  System.get_env("POSTGRES_PASSWORD") = db_pw ->
     [
       username: System.get_env("POSTGRES_USER", "postgres"),
-      password: System.get_env("POSTGRES_PASSWORD", "postgres"),
+      password: db_pw,
       hostname: System.get_env("POSTGRES_HOST", "localhost"),
       database: "bonfire_test_#{test_instance}_#{System.get_env("MIX_TEST_PARTITION")}",
       socket_options: maybe_repo_ipv6
     ]
+    true -> 
+      nil
   end
   
 # Choose password hashing backend
@@ -61,13 +63,15 @@ config :needle, :search_path, []
 config :bonfire, :endpoint_module, Bonfire.Web.Endpoint
 config :phoenix_test, :endpoint, Bonfire.Web.Endpoint
 
-config :bonfire, :repo_module, Bonfire.Common.Repo
-config :bonfire_umbrella, Bonfire.Common.Repo, repo_connection_config
-config :bonfire_umbrella, Bonfire.Common.TestInstanceRepo, repo_connection_config
-config :bonfire, ecto_repos: [Bonfire.Common.Repo]
-config :bonfire_umbrella, ecto_repos: [Bonfire.Common.Repo]
-config :paginator, ecto_repos: [Bonfire.Common.Repo]
-config :activity_pub, ecto_repos: [Bonfire.Common.Repo]
+if repo_connection_config do
+  config :bonfire, :repo_module, Bonfire.Common.Repo
+  config :bonfire_umbrella, Bonfire.Common.Repo, repo_connection_config
+  config :bonfire_umbrella, Bonfire.Common.TestInstanceRepo, repo_connection_config
+  config :bonfire, ecto_repos: [Bonfire.Common.Repo]
+  config :bonfire_umbrella, ecto_repos: [Bonfire.Common.Repo]
+  config :paginator, ecto_repos: [Bonfire.Common.Repo]
+  config :activity_pub, ecto_repos: [Bonfire.Common.Repo]
+end
 
 config :ecto_sparkles, :otp_app, :bonfire
 config :ecto_sparkles, :env, config_env()
