7b5d74d91dc6edc6887ef77b92891f64212b2fd0
Mayel de Borniol <git@mayel.space>
2024-12-16 19:03:54 +0000

    fix

--- lib/web/fake_remote_endpoint.ex
+++ lib/web/fake_remote_endpoint.ex
@@ -3,4 +3,7 @@ defmodule Bonfire.Web.FakeRemoteEndpoint do
   use Bonfire.UI.Common.EndpointTemplate
 
   plug(Bonfire.Web.Router)
+
+  defdelegate include_assets(conn), to: Bonfire.Web.Endpoint
+  defdelegate include_assets(conn, section), to: Bonfire.Web.Endpoint
 end
